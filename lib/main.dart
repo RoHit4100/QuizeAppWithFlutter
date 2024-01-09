a9de638f855ab6aefc9c42c58583334a9bdc70ed
RoHit4100 <rohitudamale777@gmail.com>
2024-01-09 16:00:51 +0530

    some initial styling is done

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,23 @@
 import "package:flutter/material.dart";
 import "package:quizeapp/start_screen.dart";
-void main(){
+
+void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       title: "My App",
       home: Scaffold(
-        body: StartScreen(),
+        body: Container(
+          decoration:const BoxDecoration(
+              gradient: LinearGradient(
+                  colors: [
+                  Color.fromARGB(255, 183, 41, 140),
+                  Color.fromARGB(255, 148, 85, 132)],
+                  begin: Alignment.topCenter, 
+                  end: Alignment.bottomCenter),
+                  ),
+          child: const StartScreen(),
+        ),
       ),
-    )
+    ),
   );
 }
--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -1,11 +1,27 @@
 import "package:flutter/material.dart";
 
-class StartScreen extends StatelessWidget{
-  const StartScreen({Key? key}): super(key: key);
+class StartScreen extends StatelessWidget {
+  const StartScreen({Key? key}) : super(key: key);
   @override
-  Widget build(BuildContext context){
-    return const Center(
-      child: Text("Start Screen"),
-    );
+  Widget build(BuildContext context) {
+    return Center(
+        child: Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Image.asset(
+          "assets/images/quiz-logo.png",
+          width: 300,
+          height: 300,
+        ),
+        const SizedBox(height: 30),
+        const Text("Welcome to this project",
+            style: TextStyle(fontSize: 23, color: Colors.white)),
+        const SizedBox(height: 30),
+        ElevatedButton(
+          onPressed: () {},
+          child: const Text("Start Quiz"),
+        ),
+      ],
+    ));
   }
 }
